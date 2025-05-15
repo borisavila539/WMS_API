19f424aa350d02eb262114d719c8dcefd62620a1
gmeza <gmeza@intermoda.com.hn>
2025-05-15 14:36:47 -0600

    feat (RecTela): Marcar rollos como enviados

---
+++ Core/DTOs/IM_WMS_RecTela/RecTelaByVendroll/IM_WMS_RecTela_PostIsSendByActivityUUIDDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs.IM_WMS_RecTela.RecTelaByVendroll
+{
+    public class IM_WMS_RecTela_PostIsSendByActivityUUIDDTO
+    {
+        public string TelaPickingByVendrollId { get; set; }
+        public string VendRoll { get; set; }
+        public string ProveedorId { get; set; }
+        public string Location { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public Guid ActivityUUID { get; set; }
+        public int TelaPickingTypeId { get; set; }
+        public bool IsSend { get; set; }
+    }
+}
