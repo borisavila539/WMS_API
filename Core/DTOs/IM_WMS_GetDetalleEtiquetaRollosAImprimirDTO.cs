9de11742e434449c63fdc12515a795065ca75764
eliBueso <107525077+eliBueso@users.noreply.github.com>
2026-05-15 10:21:44 -0600

    merge de cambios del master (#35)
    * colocar  nombre del contratista
    
    * Agregado api impresion etiquetas rollos (#32)
    
    * cambios para impresion etiquetas de rollo
    
    * campos que faltaban para etiquteta de Denim
    
    * fix por error en nueva pantalla de impresion etiquetas tejido de punto y denim (#34)
    
    ---------
    
    Co-authored-by: borisavila539 <68365374+borisavila539@users.noreply.github.com>

---
+++ Core/DTOs/IM_WMS_GetDetalleEtiquetaRollosAImprimirDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public class IM_WMS_GetDetalleEtiquetaRollosAImprimirDTO
+    {
+        public string Tela { get; set; }
+        public string NombreBusqueda { get; set; }
+        public string Color { get; set; }
+        public string NombreColor { get; set; }
+        public string Configuracion { get; set; }
+        public string NumeroRollo { get; set; }
+        public string NumeroRolloProveedor { get; set; }
+        public string Proveedor { get; set; }
+        public string NombreProveedor { get; set; }
+        public string Almacen { get; set; }
+        public string Lote { get; set; }
+        public string Ubicacion { get; set; }
+        public decimal Cantidad { get; set; }
+        public string Unidad { get; set; }
+        public string Auditor { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string Comentario { get; set; }
+        public string PPM2 { get; set; }
+    }
+}
